1ad42d2d602d9099904d9116b8f8a2888a672d7c
antoniomusu <antonio.musumarra@hotmail.com>
2023-04-16 20:50:52 +0200

    First commit

---
+++ mhw3.js
@@ -0,0 +1,77 @@
+ const nutrition_key = "cd8cd546dc2f41d09527b1225934f538";
+
+function onResponse(response){
+    console.log("Richiesta ricevuta");
+    return response.json();
+}
+
+
+function onRecipeJson(recipe){
+    console.log(recipe);
+    openModel(recipe);
+    
+}
+
+function openModel(recipe){
+    const modal = document.querySelector("#modal");
+    modal.innerHTML = " ";
+    modal.style.top = window.pageYOffset + "px";
+    img = document.createElement("img");
+    img.src = recipe.image;
+    textRecipe= document.createElement("div");
+    link = document.createElement("a");
+    link.textContent = recipe.title;
+    link.href= recipe.sourceUrl;
+    text = document.createElement("p");
+    text.textContent = "Tempo di preparazione: " + recipe.readyInMinutes + " minuti\r\n" + "Persone: " + recipe.servings + "\r\nSenza glutine: " + recipe.glutenFree + "\r\nVegetariano: " + recipe.vegetarian  +"\r\nIngredienti:";
+    for(let ingredient of recipe.extendedIngredients){
+    text.textContent += "\r\n" + ingredient.name;
+    }
+
+    modal.appendChild(img);
+    textRecipe.appendChild(link);
+    textRecipe.appendChild(text);
+    modal.appendChild(textRecipe);
+    modal.classList.remove("hidden");
+
+    document. addEventListener("keydown", function(event) {
+        if(event.key === "Escape"){
+            modal.classList.add("hidden");
+            document.body.classList.remove("no-scroll");
+        }
+        });
+}
+
+function clickRecipe(event){
+    event.stopPropagation();
+    document.body.classList.add("no-scroll");
+    fetch("https://api.spoonacular.com/recipes/"+event.currentTarget.id+"/information?"+"apiKey="+ nutrition_key).then(onResponse).then(onRecipeJson);
+}
+
+function onJson(json){
+    console.log(json);
+   const section = document.querySelector(".content"); 
+   section.innerHTML = " ";
+    for(let food of json.results){
+        let result = document.createElement("div");
+        result.id = food.id;
+        let food_img = document.createElement("img");
+        let food_title = document.createElement("h1");
+        food_title.textContent = food.title;
+        food_img.src = food.image;
+        result.appendChild(food_title);
+        result.appendChild(food_img);
+        section.appendChild(result);
+        result.addEventListener('click', clickRecipe);
+    }
+}
+
+function search(event){
+    console.log("Ricerca");
+    event.stopPropagation();
+    event.preventDefault();
+    const content = encodeURIComponent(document.querySelector("#search").value); 
+    fetch("https://api.spoonacular.com/recipes/complexSearch?"+"apiKey="+ nutrition_key+ "&query="+content+"&number=100").then(onResponse).then(onJson);
+}
+const form = document.querySelector(".navbar form");
+form.addEventListener("submit", search);
