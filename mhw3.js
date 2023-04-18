174803f10f27f548647362745d79e229736406b8
antoniomusu <antonio.musumarra@hotmail.com>
2023-04-18 13:08:39 +0200

    Final Commit - 2 ApiKey

--- mhw3.js
+++ mhw3.js
@@ -1,58 +1,62 @@
- const nutrition_key = "cd8cd546dc2f41d09527b1225934f538";
-
-function onResponse(response){
+const nutrition_key = "cd8cd546dc2f41d09527b1225934f538";
+function onResponse(response) {
     console.log("Richiesta ricevuta");
     return response.json();
 }
 
 
-function onRecipeJson(recipe){
+function onRecipeJson(recipe) {
     console.log(recipe);
-    openModel(recipe);
-    
+    openModal(recipe);
+
 }
 
-function openModel(recipe){
+
+function openModal(recipe) {
     const modal = document.querySelector("#modal");
     modal.innerHTML = " ";
     modal.style.top = window.pageYOffset + "px";
-    img = document.createElement("img");
+    let img = document.createElement("img");
     img.src = recipe.image;
-    textRecipe= document.createElement("div");
-    link = document.createElement("a");
+    textRecipe = document.createElement("div");
+    let link = document.createElement("a");
     link.textContent = recipe.title;
-    link.href= recipe.sourceUrl;
-    text = document.createElement("p");
-    text.textContent = "Tempo di preparazione: " + recipe.readyInMinutes + " minuti\r\n" + "Persone: " + recipe.servings + "\r\nSenza glutine: " + recipe.glutenFree + "\r\nVegetariano: " + recipe.vegetarian  +"\r\nIngredienti:";
-    for(let ingredient of recipe.extendedIngredients){
-    text.textContent += "\r\n" + ingredient.name;
+    link.href = recipe.sourceUrl;
+    let like = document.createElement("img");
+    like.id="like";
+    like.src = "heart-unchecked.png";
+    let text = document.createElement("p");
+    text.textContent = "Tempo di preparazione: " + recipe.readyInMinutes + " minuti\r\n" + "Persone: " + recipe.servings + "\r\nSenza glutine: " + recipe.glutenFree + "\r\nVegetariano: " + recipe.vegetarian + "\r\nIngredienti:";
+    for (let ingredient of recipe.extendedIngredients) {
+        text.textContent += "\r\n" + ingredient.name;
     }
-
+    
     modal.appendChild(img);
     textRecipe.appendChild(link);
+    textRecipe.appendChild(like);
     textRecipe.appendChild(text);
     modal.appendChild(textRecipe);
     modal.classList.remove("hidden");
 
-    document. addEventListener("keydown", function(event) {
-        if(event.key === "Escape"){
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Escape") {
             modal.classList.add("hidden");
             document.body.classList.remove("no-scroll");
         }
-        });
+    });
 }
 
-function clickRecipe(event){
+function clickRecipe(event) {
     event.stopPropagation();
     document.body.classList.add("no-scroll");
-    fetch("https://api.spoonacular.com/recipes/"+event.currentTarget.id+"/information?"+"apiKey="+ nutrition_key).then(onResponse).then(onRecipeJson);
+    fetch("https://api.spoonacular.com/recipes/" + event.currentTarget.id + "/information?" + "apiKey=" + nutrition_key).then(onResponse).then(onRecipeJson);
 }
 
-function onJson(json){
+function onJson(json) {
     console.log(json);
-   const section = document.querySelector(".content"); 
-   section.innerHTML = " ";
-    for(let food of json.results){
+    const section = document.querySelector(".content");
+    section.innerHTML = " ";
+    for (let food of json.results) {
         let result = document.createElement("div");
         result.id = food.id;
         let food_img = document.createElement("img");
@@ -66,12 +70,14 @@ function onJson(json){
     }
 }
 
-function search(event){
+function search(event) {
     console.log("Ricerca");
     event.stopPropagation();
     event.preventDefault();
-    const content = encodeURIComponent(document.querySelector("#search").value); 
-    fetch("https://api.spoonacular.com/recipes/complexSearch?"+"apiKey="+ nutrition_key+ "&query="+content+"&number=100").then(onResponse).then(onJson);
+    const content = encodeURIComponent(document.querySelector("#search").value);
+    fetch("https://api.spoonacular.com/recipes/complexSearch?" + "apiKey=" + nutrition_key + "&query=" + content + "&number=100")
+        .then(onResponse)
+        .then(onJson);
 }
 const form = document.querySelector(".navbar form");
 form.addEventListener("submit", search);
